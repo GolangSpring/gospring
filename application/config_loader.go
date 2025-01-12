cb89d812388b51d76707d1c4764ae125ca852b00
William Chen <william_w_chen@trendmicro.com>
2025-01-12 14:46:13 +0800

    refactor: use log message to stop program execution with meaningful error message.

--- application/config_loader.go
+++ application/config_loader.go
@@ -1,6 +1,7 @@
 package application
 
 import (
+	"github.com/rs/zerolog/log"
 	"gopkg.in/yaml.v3"
 	"os"
 )
@@ -8,11 +9,11 @@ import (
 func MustNewConfigFromFile[T any](configPath string) *T {
 	file, err := os.ReadFile(configPath)
 	if err != nil {
-		panic(err)
+		log.Fatal().Msgf("Failed to read config file: %v", err)
 	}
 	var config T
 	if err := yaml.Unmarshal(file, &config); err != nil {
-		panic(err)
+		log.Fatal().Msgf("Failed to unmarshal config: %v", err)
 	}
 	return &config
 }
