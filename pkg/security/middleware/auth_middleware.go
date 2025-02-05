b9935d0c4ef6f72db579be045a7883dc39e643df
William Chen <william_w_chen@trendmicro.com>
2025-02-06 01:00:17 +0800

    feat: Enhance Auth Middleware to Support Bearer Token Authentication
    - Added support for extracting and validating Bearer tokens from the Authorization header.
    - Fallback to token extraction from cookies if Authorization header is not present.
    - Improved error handling and logging for invalid Authorization headers.
    - Updated context key for user information to a constant `UserContextKey`.

--- pkg/security/middleware/auth_middleware.go
+++ pkg/security/middleware/auth_middleware.go
@@ -3,8 +3,16 @@ package middleware
 import (
 	"context"
 	"fmt"
+	"github.com/GolangSpring/gospring/helper"
 	"github.com/GolangSpring/gospring/pkg/security/service"
+	"github.com/rs/zerolog/log"
 	"net/http"
+	"strings"
+)
+
+const (
+	EmptyString    = ""
+	UserContextKey = "user"
 )
 
 type AuthMiddleware struct {
@@ -23,20 +31,47 @@ func (middleware *AuthMiddleware) Middleware(next http.Handler) http.Handler {
 			return
 		}
 
-		token, err := r.Cookie("token")
-		if err != nil {
-			http.Error(w, "Missing token, please login first", http.StatusUnauthorized)
+		var tokenFound string
+		authToken := r.Header.Get("Authorization")
+		if authToken != EmptyString {
+			parts := strings.SplitN(authToken, " ", 2)
+			var bearerPrefix, token string
+			if err := helper.SliceUnpack(parts, &bearerPrefix, &token); err != nil {
+				errString := fmt.Sprintf("Invalid Authorization header: %v", err)
+				log.Warn().Msg(errString)
+				http.Error(w, errString, http.StatusBadRequest)
+				return
+			}
+
+			if strings.EqualFold(bearerPrefix, "Bearer") {
+				tokenFound = token
+			} else {
+				log.Warn().Msg("Authorization header does not contain 'Bearer' prefix")
+				http.Error(w, "Invalid Authorization header format", http.StatusBadRequest)
+				return
+			}
+		}
+
+		// Fallback to cookie if no Authorization header
+		if tokenFound == EmptyString {
+			if token, err := r.Cookie("token"); err == nil {
+				tokenFound = token.Value
+			}
+		}
+
+		if tokenFound == EmptyString {
+			http.Error(w, "Missing token, unauthorized.", http.StatusUnauthorized)
 			return
 		}
 
 		// Token is valid - set custom headers or context (e.g., user info)
 		// Add user info to the request context
-		userClaims, err := middleware.AuthService.ParseUserClaims(token.Value)
+		userClaims, err := middleware.AuthService.ParseUserClaims(tokenFound)
 		if err != nil {
 			http.Error(w, fmt.Sprintf("Invalid token: %v", err), http.StatusUnauthorized)
 			return
 		}
-		ctx := context.WithValue(r.Context(), "user", userClaims)
+		ctx := context.WithValue(r.Context(), UserContextKey, userClaims)
 
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
